a450e07736565d0d26222fe5c2ce4d3368ae4fb9
RachelL2018 <36459194+RachelL2018@users.noreply.github.com>
2018-04-04 23:10:15 +0100

    Add files via upload

---
+++ wk6.py
@@ -0,0 +1,15 @@
+
+
+
+def factorial(num):
+
+   sum = num
+   for i in range(1, num):
+        sum = sum * i
+   return sum
+
+
+print(" the factorial is:",factorial(num= int(input("Enter first number:"))))
+
+
+
