4395afbf5c02c50de46f2bf3e58c0ce9d914749c
RachelL2018 <36459194+RachelL2018@users.noreply.github.com>
2018-04-04 23:11:23 +0100

    Add files via upload

---
+++ wk4.py
@@ -0,0 +1,19 @@
+#Rachel Leonard
+
+divide = range(1,21)
+num = 1
+while num < 100:
+ for d in divide:
+    num = num/d
+    #smalllest = min(num)
+    if num % 2 == 0: # and num/divide : 
+     print (num)
+
+
+#print (min(num))
+    
+  
+   
+ #(min(num2 % 2==0) and (num2 > 0))
+    # 2520 is the smallest number divisible by all numbers 1 to 10
+
