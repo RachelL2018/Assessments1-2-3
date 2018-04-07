148cfd84d3f41cf057fe9f0b67bd453879a88f20
RachelL2018 <36459194+RachelL2018@users.noreply.github.com>
2018-04-08 00:07:21 +0100

    Update week5.py

--- week5.py
+++ week5.py
@@ -1,4 +1,5 @@
-with open("data\iris.csv") as f:
-   for line in f:
+with open("data\iris.csv") as f: #open function to open a csv file by passing the path name to it
+   for line in f:# loops through each line in file
     
-    print(line.split(',')[0:4])
+    print(line.split(',')[0:4])# print each line in file as it loops through & uses split method to return an array of strings in the file and 
+                               # returns the items in the array ranging from indexes specified.
