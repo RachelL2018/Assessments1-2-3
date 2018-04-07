7aa0047d5238ddb0971e83ebdc876f649204caa0
RachelL2018 <36459194+RachelL2018@users.noreply.github.com>
2018-04-07 23:38:43 +0100

    Update euler_5.py

--- euler_5.py
+++ euler_5.py
@@ -1,23 +1,20 @@
-
-
-
 #Rachel Leonard/Euler problem 5 solution
 
-#create a function that takes in argument num
+#defines a function that the parameter num is passed to
 def divisable(num):
-#for loop which loops through 1 to 20
+#for loop which loops 20 times. It also loops through 1 to 20 assigning i a value which increments each time it loops 
     for i in range(1,21):
         if num % i != 0:# it divides the varaible num by i each time it loops.It also states a condition that if each time num is divided by i it is not even it returns a boolean of false
             return False
     return True
-#defines a varaible called num and sets it 1
-num = 1#while loop which loops until True
-while True:
-    if divisable(num): #calls the function divisable and stops when True
-        break
-    num += 1 #the varaible increments each time it loops
 
-print(num)# prints the number 
+num = 1 # defines a varaible called num and sets it to 1
+while True: #while loop which loops until true
+    if divisable(num): #calls the function divisable and stops if True is returned from the function
+        break
+    num += 1 #sets the varaible to increment each time it loops
+
+print(num)# prints the value stored in num.
 
 
         
