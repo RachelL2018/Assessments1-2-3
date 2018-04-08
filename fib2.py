f26b7cb3907b422c5b38815282a03dc54ce712d3
RachelL2018 <36459194+RachelL2018@users.noreply.github.com>
2018-04-08 18:26:50 +0100

    Update fib2.py

--- fib2.py
+++ fib2.py
@@ -13,15 +13,15 @@ def fib(n):
   
   return i
 
-name = input("enter your surname  ")
-first = name[0]
-last = name[-1]
-firstno = ord(first)
-lastno = ord(last)
-x = firstno + lastno
+name = input("enter your surname  ") # creates a variable which takes in a string from the user
+first = name[0] # creates a varaible which gets the first character from the varaible name and stores it
+last = name[-1] # creates a varaible which gets the last character from the varaible name and stores it
+firstno = ord(first)#creates a varaible which stores the value of the integer that represents the unicode point of the character stored in firstno using ord fucntion to obtian is 
+lastno = ord(last)#creates a varaible which stores the value of the integer that represents the unicode of the character stored in lastno using ord function to obtain it
+x = firstno + lastno#creates a varaible which stores that value of the sum of the tow variables firstno and lastno
 
-ans = fib(x)
+ans = fib(x)# creates a variable which stores the result of the called function fib which passed the parameter x to it
 print("My surname is", name)
 print("The first letter", first, "is number", firstno)
 print("The last letter", last, "is number", lastno)
-print("Fibonacci number", x, "is", ans)
+print("Fibonacci number", x, "is", ans)# prints  a string and the value of the varibke x and the value of the ans which contains the result of the called function.
